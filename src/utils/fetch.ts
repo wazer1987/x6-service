3c17b82494c8d7b49acd766b57b859159b621e24
王正 <wangzheng@git.com.cn>
2023-05-08 10:09:33 +0800

    更改

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
-const apis = 'http://10.10.1.82:8124'
-
+// const apis = 'http://10.10.1.82:8124'
+const apis = ' http://10.100.3.16:8124'
+//
 export const getNodeList = ():any => {
   return axios.get(apis + '/tbCamelInfo/findNode?')
 }
